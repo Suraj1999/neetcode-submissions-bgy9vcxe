bfe7db65e8f836b1c925a2962f4c796fc47d3b5a
Suraj1999 <102948555+Suraj1999@users.noreply.github.com>
2026-03-30 15:36:06 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func groupAnagrams(_ strs: [String]) -> [[String]] {
+       
+       var strDict = [[Int]: [String]]()
+
+       for str in strs {
+          
+          var count = Array(repeating: 0, count: 26)
+
+          for s in str {
+            let index = Int(s.asciiValue! - Character("a").asciiValue!)
+            count[index] += 1
+          }
+
+          strDict[count, default:[]].append(str)
+       }
+
+       return Array(strDict.values)
+    }
+}
